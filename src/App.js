0753b2452a61f2dc2c855f5ef03ef3e27379b972
Abozarv <vakiliabozar@gmail.com>
2025-01-07 03:59:00 +0330

    update theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -7,10 +7,34 @@ import Cart from './Components/Cart';
 import ContactUs from './Components/ContactUs';
 import About from './Components/About';
 import Profile from './Components/Profile';
+
+import { FaSun, FaMoon } from "react-icons/fa";
+
 const App = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  useEffect(() => {
+    // بازیابی تم از localStorage
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      setIsDarkTheme(true);
+      document.body.classList.add("dark-theme");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = !isDarkTheme;
+    setIsDarkTheme(newTheme);
+    document.body.classList.toggle("dark-theme", newTheme);
+    localStorage.setItem("theme", newTheme ? "dark" : "light"); // ذخیره تم
+  };
   return (
     <Router>
       <Header />
+      <div className="app-container">
+      <button onClick={toggleTheme} className="theme-toggle-button">
+        {isDarkTheme ? <FaSun size={24} color="#FFD700" /> : <FaMoon size={24} color="#4C4C4C" />}
+      </button></div>
 
       <Routes>
         <Route path="/" element={<Home />} />
